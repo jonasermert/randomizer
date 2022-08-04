aa7125c54ef574e3c0fec4ad69064a60952be151
Jonas <ermert.jonas@yahoo.de>
2022-08-04 11:23:36 +0200

    RangeSelectorPage Outline

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -10,6 +10,20 @@ class RangeSelectorPage extends StatefulWidget {
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Range'),
+      ),
+      body: Form(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_forward),
+          onPressed: () {
+            // TODO: Validate the Form
+            // TODO: Navigate to the generator page
+          }),
+    );
   }
 }
